926917b0f3a90a7f47a1d7e899db8c271829f375
Theo Sandell <theo01sandell@gmail.com>
2021-02-25 02:06:46 +0100

    Added backup/fallback glyph fonts

--- config.h
+++ config.h
@@ -5,12 +5,16 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=13:antialias=true:autohint=true";
 
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"FontAwesome5Free:pixelsize=13:antialias=true:autohint=true",
+	"FontAwesome5Solid:pixelsize=13:antialias=true:autohint=true"
+	"FontAwesome5FreeBrands:pixelsize=13:antialias=true:autohint=true"
+	"MesloLGS NF:pixelsize=13:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
